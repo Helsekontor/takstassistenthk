0d38e78dede2034fc4bf92b78056877cc339b06b
Helsekontor <mikkel.owre@helsekontor.no>
2026-06-19 14:50:33 +0200

    Delete Main directory

--- Main/register.js
+++
@@ -1,7 +0,0 @@
-{
-  "rewrites": [
-    { "source": "/api/chat",     "destination": "/api/chat.js" },
-    { "source": "/api/register", "destination": "/api/register.js" },
-    { "source": "/(.*)",         "destination": "/public/$1" }
-  ]
-}
